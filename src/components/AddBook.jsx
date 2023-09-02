30a5239cfda066e2392e3a86935ee2823e601c23
BiancaMe <bianquis0410@gmail.com>
2023-09-01 22:19:30 -0300

    Add Remove and Add  a new Book functionalities

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,17 +1,51 @@
-const AddNewBook = () => (
-  <div>
-    <h3>Add New Book</h3>
-    <form>
-      <input type="text" className="title-form" placeholder="Book title" />
-      <select className="author-form" placeholder="Author">
-        <option value="action">Susan Collins</option>
-        <option value="fiction">Franks</option>
-        <option value="economy">Collins</option>
-        <option value="comedy">Frank Herbert</option>
-      </select>
-      <button type="submit">ADD BOOK</button>
-    </form>
-  </div>
-);
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
+import { addBook } from '../redux/books/bookSlice';
+
+const AddNewBook = () => {
+  const dispatch = useDispatch();
+  const [data, setData] = useState({
+    id: '-1', title: '', author: 'Susan Collins', category: 'None',
+  });
+  const list = useSelector((state) => state.book.bookList);
+  const setTitle = (e) => {
+    setData((values) => ({
+      ...values,
+      title: e.target.value,
+      id: `${list.length + 1}`,
+    }));
+  };
+  const setAuthor = (e) => {
+    setData((values) => ({
+      ...values,
+      author: e.target.value,
+    }));
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    dispatch(addBook(data));
+  };
+
+  return (
+    <div>
+      <h3>Add New Book</h3>
+      <form onSubmit={submit}>
+        <input type="text" className="title-form" placeholder="Book title" onChange={setTitle} />
+        <select
+          className="author-form"
+          placeholder="Author"
+          onChange={setAuthor}
+        >
+          <option value="Susan Collins">Susan Collins</option>
+          <option value="Franks">Franks</option>
+          <option value="Collins">Collins</option>
+          <option value="Frank Herbert">Frank Herbert</option>
+        </select>
+        <button type="submit">ADD BOOK</button>
+      </form>
+    </div>
+  );
+};
 
 export default AddNewBook;
